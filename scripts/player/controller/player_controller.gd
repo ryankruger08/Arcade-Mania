6e95cf96646e54bbc5b0a02183e2cd66f4869fc3
Ryan Kruger <105048412+ryankruger08@users.noreply.github.com>
2026-08-14 12:24:05 +1000

    Initial commit

---
+++ scripts/player/controller/player_controller.gd
@@ -0,0 +1,53 @@
+extends CharacterBody3D
+
+
+@export var speed: float = 5.0
+@export var jump_velocity: float = 4.5
+@export var mouse_sensitivity: float = 0.003
+
+
+@export var min_pitch: float = deg_to_rad(-85)
+@export var max_pitch: float = deg_to_rad(85)
+
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+@onready var camera: Camera3D = $Camera3D
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		
+		camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		
+		camera.rotation.x = clamp(camera.rotation.x, min_pitch, max_pitch)
+		
+	if event.is_action_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
