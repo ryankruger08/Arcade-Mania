a21a552991341979ed15a71aee24ea51541c4945
Ryan Kruger <105048412+ryankruger08@users.noreply.github.com>
2026-08-18 10:26:59 +1000

    hehe

--- scripts/player/controller/player_controller.gd
+++ scripts/player/controller/player_controller.gd
@@ -36,10 +36,10 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
